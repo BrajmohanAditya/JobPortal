5dc5464873118d005f2f392e839aa9628028289f
Mohan Kumar <cckumarsingh38ar@gmail.com>
2026-04-29 14:31:01 +0530

    change

--- services/user/src/controllers/user.ts
+++ services/user/src/controllers/user.ts
@@ -3,7 +3,7 @@ import { AuthenticatedRequest } from "../middleware/auth.js";
 
 export const myProfile = TryCatch(async (req:AuthenticatedRequest,res,next)=>{
     const user = req.user;
-
+ 
    res.json(user);
     
 })
